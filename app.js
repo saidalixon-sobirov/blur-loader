3a64336915026f90dabe8a6353ae40776c9e80d2
saidalixon-sobirov <saidalixon_sobirov@mail.ru>
2022-08-29 00:29:45 +0500

    backdrop blur loading

---
+++ app.js
@@ -0,0 +1,26 @@
+// saidalixon.uz
+
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const loadingTxt = document.querySelector(".loading"),
+    bgImg = document.querySelector(".bg");
+
+  let load = 0;
+
+  const loadInterval = setInterval(() => {
+    addLoad();
+  }, 50);
+
+  function addLoad() {
+    load++;
+
+    if (load > 99) {
+      clearInterval(loadInterval);
+    }
+
+    loadingTxt.textContent = `${load}%`;
+    loadingTxt.style.opacity = 100 - load + "%";
+    bgImg.style.backdropFilter = `blur(${100 - load}px)`;
+  }
+});
